11f746997e18abd748c194dae6b5de964b010bc2
marcin1997garbarczyk <marcin1997garbarczyk@gmail.com>
2024-06-11 22:14:11 +0200

    login_and_register_init

--- frontendApp/urls.py
+++ frontendApp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('myStories', views.myStories, name="myStories"),
+    path('myStories', views.my_stories, name="myStories"),
     path('story/<int:id>', views.story, name="story"),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.user_register, name='register'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- frontendApp/views.py
+++ frontendApp/views.py
@@ -1,10 +1,44 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignupForm, LoginForm
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     return render(request, 'home.html')
-def myStories(request):
+@login_required(login_url='login')
+def my_stories(request):
     return render(request, 'mystories.html')
 
+@login_required(login_url='login')
 def story(request, id):
     return render(request, 'storyPage.html')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+def user_logout(request):
+    logout(request)
+    return redirect('login')
