1f67d9d0d6fec76cafca6e0ac01e637f4cf9a345
marcin1997garbarczyk <marcin1997garbarczyk@gmail.com>
2024-06-12 18:51:35 +0200

    login_and_register_init

--- frontendApp/forms.py
+++ frontendApp/forms.py
@@ -5,8 +5,18 @@ from django.contrib.auth.models import User
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.fields['username'].widget.attrs.update({'class': 'form-control'})
+       self.fields['email'].widget.attrs.update({'class': 'form-control'})
+       self.fields['password1'].widget.attrs.update({'class':'form-control'})
+       self.fields['password2'].widget.attrs.update({'class':'form-control'})
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.fields['username'].widget.attrs.update({'class': 'form-control'})
+       self.fields['password'].widget.attrs.update({'class':'form-control'})
